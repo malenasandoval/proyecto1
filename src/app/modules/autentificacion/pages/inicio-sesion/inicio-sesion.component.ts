3f43cdaaa05f82dfeb43f7bad6280a5326e568b6
malenasandoval <malenasando@gmail.com>
2024-06-29 23:54:55 -0300

    aplicamos responsividad con medias querys en el navegador

--- src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from '../../services/auth.service';
+import { FirestoreService } from 'src/app/modules/shared/services/firestore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -7,61 +10,124 @@ import { Usuario } from 'src/app/models/usuario';
   styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
-  // Este "hide" es para el input de contraseña
   hide = true;
 
-  // arreglo publico donde traigo la interfaz Usuario
-  public usuarioLocal : Usuario[]
-
-  // Utilizamos el constructor para inicializar usuarioLocal con una lista predefinida de usuarios
-  constructor() {
-    this.usuarioLocal = [{
-      uid: '', 
-      nombre: 'Malena',
-      apellido: 'Sandoval',
-      email: 'malenasando@gmail.com',
-      rol: 'visitante',
-      password: 'malena05',
-    }]
+  // ####################################### LOCAL
+  /* ####################################### LOCAL
+  // Definimos la propiedad local para que guarde la colección
+  // COLECCIÓN LOCAL DE usuarioIngresado CON INFORMACIÓN
+  public coleccionusuarioIngresadoLocal: Usuario[];
+  
+  constructor(){
+    this.coleccionusuarioIngresadoLocal = [
+      {
+        uid: '',
+        nombre: 'Leandro',
+        apellido: 'Soto',
+        email: 'leandrosoto@gmail.com',
+        rol: 'admin',
+        password: '123456'
+      },
+      {
+        uid: '',
+        nombre: 'Pepe',
+        apellido: 'Novita',
+        email: 'pepenovita@gmail.com',
+        rol: 'vis',
+        password: 'abc123'
+      },
+      {
+        uid: '',
+        nombre: 'Tomas',
+        apellido: 'Loyola',
+        email: 'tomasloyola@gmail.com',
+        rol: 'admin',
+        password: 'abcdef'
+      }
+    ]
   }
 
-  // Objeto para almacenar los datos del usuario que intenta iniciar sesion
-  usuarios: Usuario = {
+  }*/
+  // ####################################### FIN LOCAL
 
-    uid: '', // -> comillas vacias porque es string
+  constructor(
+    public servicioAuth: AuthService,
+    public servicioFirestore: FirestoreService,
+    public servicioRutas: Router
+  ) { }
+
+  // ####################################### INGRESADO
+  // Importamos la interfaz de usuario e inicializamos vacío
+  usuarioIngresado: Usuario = {
+    uid: '',
     nombre: '',
     apellido: '',
     email: '',
     rol: '',
-    password: '',
-  };
+    password: ''
+  }
 
-  // Creamos una coleccion para almacenar multiples usuarios de tipo usuario (inicialmente vacia)
-  coleccionInicioSesion: Usuario []= [];
-
-  //Creamos la funcion para registrar (o validar) un incio de sesion
-  IniciarSesion (){
-
-    //constante credenciales va a guardar la informacion que ingrese el usuario
+  // Función para el inicio de sesión
+  async iniciarSesion() {
+    // ############################################# LOCAL
+    // Las credenciales reciben la información que se envía desde la web
+    /*
     const credenciales = {
-      uid: this.usuarios.uid,
-      nombre: this.usuarios.nombre,
-      apellido: this.usuarios.apellido,
-      email: this.usuarios.email,
-      rol: this.usuarios.rol,
-      password: this.usuarios.password,
+      uid: this.usuarioIngresado.uid,
+      nombre: this.usuarioIngresado.nombre,
+      apellido: this.usuarioIngresado.apellido,
+      email: this.usuarioIngresado.email,
+      rol: this.usuarioIngresado.rol,
+      password: this.usuarioIngresado.password
     }
-
-    //Crear un ciclo que recorra los ingresos de los usuarios
-    for (let i = 0; i < this.usuarioLocal.length; i++) {
-      //Creamos una constante que vamos a utilizar para verificar la informacion
-      const orden = this.usuarioLocal[i];
-
-      //Se comparan los datos ingresados con los guardados
-      if (orden.email === credenciales.email && orden.password === credenciales.password) {
-        alert ("Ha iniciado sesion")
+    // Repetitiva para recorrer la colección local
+    for(let i = 0; i < this.coleccionusuarioIngresadoLocal.length; i++){
+      // Constante que guarde la información de la posición actual de los objetos
+      const usuarioLocal = this.coleccionusuarioIngresadoLocal[i];
+      
+      Comparando uno por uno los atributos del objeto local con el que ingresa el 
+      usuario 
+      if(usuarioLocal.nombre === credenciales.nombre && 
+        usuarioLocal.apellido === credenciales.apellido && 
+        usuarioLocal.email === credenciales.email && 
+        usuarioLocal.rol === credenciales.rol && 
+        usuarioLocal.password === credenciales.password
+      ){
+        // Notificamos al usuario su correcto ingreso
+        alert("Iniciaste sesión correctamente :)");
+        // Paramos la función
+        break;
       } else {
-        alert ("No ha iniciado sesion")
+        alert("No se pudo iniciar sesión :(");
+        break;
       }
+    }*/
+
+    // ############################################# FIN LOCAL
+
+    const credenciales = {
+      email: this.usuarioIngresado.email,
+      password: this.usuarioIngresado.password
     }
-}}
+
+    const res = await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.password)
+      .then(res => {
+        alert('¡Se ha logueado con éxito! :D');
+
+        this.servicioRutas.navigate(['/inicio']);
+      })
+      .catch(err => {
+        alert('Hubo un problema al iniciar sesión :( ' + err);
+
+        this.limpiarInputs();
+      })
+  }
+
+  // Función para vaciar el formulario
+  limpiarInputs() {
+    const inputs = {
+      email: this.usuarioIngresado.email = '',
+      password: this.usuarioIngresado.password = ''
+    }
+  }
+}
--- src/app/modules/autentificacion/services/auth.service.ts
+++ src/app/modules/autentificacion/services/auth.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@angular/core';
-// servicio de autentificacion de firebase
-import {AngularFireAuth} from '@angular/fire/compat/auth';
-
+// Servicio de AUTENTIFICACIÓN de FIREBASE
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+
   // Referenciar Auth de Firebase para inicializarlo
   constructor(public auth: AngularFireAuth) { }
-
-  // Función para tomar UID
 
   // Función para REGISTRO
   registrar(email: string, password: string){
@@ -23,10 +20,26 @@ export class AuthService {
     // Validar el email y la contraseña
     return this.auth.signInWithEmailAndPassword(email, password);
   }
-
+  
   // Función para CERRAR SESIÓN
   cerrarSesion(){
     // Devolver una promesa vacía
     return this.auth.signOut();
+  }
+
+  // Función para tomar UID
+  async obtenerUid(){
+    // Nos va a generar una promesa, y la constante la va a capturar
+    const user = await this.auth.currentUser;
+
+    /*
+      Si el usuario no respeta la estructura de la interfaz /
+      Si tuvo problemas para el registro -> ej.: mal internet
+    */
+    if(user == null){
+      return null;
+    } else {
+      return user.uid;
+    }
   }
 }
